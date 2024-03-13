34ace6414208789933a811706afafa4f31463143
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-03-13 09:07:43 +0530

    Add files via upload

---
+++ CustomTask/CustomTask.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+ 
+public class CustomTask : Microsoft.Build.Utilities.Task
+{
+     static void Main(string[] args)
+    {}
+    [Output]
+    public string OutputVersion { get; set; }
+ 
+    public override bool Execute()
+    {
+        try
+        {
+            string version = DateTime.Now.ToString("yyyyMMddHHmmss");
+            System.IO.File.WriteAllText(OutputVersion, version);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Log.LogErrorFromException(ex);
+            return false;
+        }
+    }
+    }
+
